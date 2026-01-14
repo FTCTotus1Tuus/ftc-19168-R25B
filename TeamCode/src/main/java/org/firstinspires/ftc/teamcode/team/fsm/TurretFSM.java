4f67da5c5698fb6a7976417e0cb68b7324620b3b
FTCTutus1Tuus <ftc19168@gmail.com>
2026-01-13 19:30:56 -0600

    Revert "(WIP) Refactor turret control to improve alignment with goals; add manual camera exposure settings"
    This reverts commit 664f38e99d96ecde1f52aced29ff0bbfc7fda46f.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/fsm/TurretFSM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/fsm/TurretFSM.java
@@ -1,21 +1,13 @@
 package org.firstinspires.ftc.teamcode.team.fsm;
 
-import static com.pedropathing.math.MathFunctions.clamp;
-
-import com.qualcomm.robotcore.hardware.Servo;
-
 public class TurretFSM {
     private final DarienOpModeFSM opMode;
-    private final Servo turretServo;
-
-    private double currentTurretPosition;
 
     /**
      * Constructor for TurretFSM
      * @param opMode The main op mode instance
      */
-    public TurretFSM(DarienOpModeFSM opMode, Servo turretServo) {
-        this.turretServo = turretServo;
+    public TurretFSM(DarienOpModeFSM opMode) {
         this.opMode = opMode;
     }
 
@@ -26,24 +18,7 @@ public class TurretFSM {
      * @return The turret heading in degrees.
      */
     public double getTurretHeading(){
-        return (double) -DarienOpModeFSM.FIVE_ROTATION_SERVO_SPAN_DEG /2 + DarienOpModeFSM.FIVE_ROTATION_SERVO_SPAN_DEG * currentTurretPosition;
-    }
-
-    /**
-     * Set the turret servo position and save it to memory.
-     * @param position The desired servo position (0.0 to 1.0).
-     */
-    public void setTurretServoPosition(double position) {
-        currentTurretPosition = clamp(position, 0, 1);
-        turretServo.setPosition(currentTurretPosition);
-    }
-
-    /**
-     * Get the current turret servo position.
-     * @return The current turret servo position (0.0 to 1.0).
-     */
-    public double getCurrentTurretServoPosition() {
-        return currentTurretPosition;
+        return -900 + DarienOpModeFSM.FIVE_ROTATION_SERVO_SPAN_DEG * opMode.currentTurretPosition;
     }
 
 }
